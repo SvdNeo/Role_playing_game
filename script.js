76f97115120427f40f00423e6cf9e96aae6fb834
SvdNeo <sanjay.d@neoito.com>
2024-01-16 21:04:00 +0530

    defeatMonster,lose,winGame

--- script.js
+++ script.js
@@ -220,4 +220,20 @@ function goFight() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
   }
   
+  function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+  }
+  
+  function lose() {
+    update(locations[5]);
+  }
+  
+  function winGame() {
+    update(locations[6]);
+  }
+  
  
