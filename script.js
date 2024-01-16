183f072d26e0a52d4d42aa2bf97687469335b854
SvdNeo <sanjay.d@neoito.com>
2024-01-16 20:58:32 +0530

    created fightbeast,fightdragon,fighyslime and gofight function

--- script.js
+++ script.js
@@ -176,3 +176,11 @@ function fightDragon() {
   goFight();
 }
 
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+  }
+ 
